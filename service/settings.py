b90b3342248d5e02d5661d2cf603771ac71fb9bc
Timur-Razzakov <razzakov334@gmail.com>
2021-12-30 00:16:00 +0500

    fix problem with send_email.py

--- service/settings.py
+++ service/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["work-searches.herokuapp.com/"]
+ALLOWED_HOSTS = ["work-searches.herokuapp.com"]
 
 # Application definition
 
