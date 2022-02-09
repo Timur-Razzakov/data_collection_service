06b5a567dc83511e500dae1776e3fdd839669cc6
Timur-Razzakov <razzakov334@gmail.com>
2022-02-09 18:53:42 +0500

    added asyncio script launch, tweaked the scripter

--- scraping/tests.py
+++ scraping/tests.py
@@ -1,105 +1,104 @@
-# from django.test import TestCase
-#
-# # Create your tests here.
-# """
-# скрипт для запуска скриптов вне проекта
-# """
-#
-# # -*- coding: utf-8 -*-
-# import json
-# import os
-# import sys
-# import django
-# from django.contrib.auth import get_user_model
-#
-# #  you have to set the correct path to you settings module
-# from django.db import DatabaseError
-# from icecream import ic
-#
-# proj = os.path.dirname(os.path.abspath('manage.py'))
-# sys.path.append(proj)
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service.settings")
-# django.setup()
-#
-# """модули обязательно должны быть прописаны после 'django.setup()'"""
-# from scraping.all_parsers.remote_job import main_scraping_part
-# from scraping.all_parsers.hh_ru import get_data
-# from scraping.models import Vacancies, City, Speciality, Error
-#
-# # TODO:написать код, который получает от пользователя city and speciality Done
-# # TODO:узнать как получить из query_set нужные данные Done
-# # TODO: написать запуск всех скриптов --Done
-# # TODO: Понять как заполнить БД полученными данными Done
-# # Возвращает пользователя по умолчанию
-# User = get_user_model()
-#
-# """
-# Функция для получения всех городов и специальностей от пользователя
-# """
-#
-# # def get_info_from_user():
-# #     query_set = User.objects.filter(send_email=True)
-# #
-# #
-# # get_info_from_user()
-# """ Все скрейперы """
-# scrapers = (get_data, main_scraping_part)
-#
-#
-# def get_settings():
-#     qs = User.objects.filter(send_email=True).values()
-#     settings_lst = set((q['city_id'], q['speciality_id']) for q in qs)
-#     return settings_lst
-#
-#
-# print(get_settings())
-#
-#
-# def get_data(_settings):
-#     qs = Vacancies.objects.all().values()
-#     general_dct = {(q['city_id'], q['speciality_id']) for q in qs}
-#     ic(general_dct)
-#     speciality_city_list = []
-#     for pair in _settings:
-#         if pair in general_dct:
-#             tmp = {}
-#             tmp['city'] = City.objects.filter(id=pair[0]).first().country_name
-#             tmp['speciality'] = Speciality.objects.filter(id=pair[1]).first().name_of_specialty
-#             speciality_city_list.append(tmp)
-#     return speciality_city_list
-#
-#
-# settings = get_settings()
-# data_list = get_data(settings, )
-# print(data_list.city)
-#
-# results, errors = [], []
-# # Запуск скрейперов
-# for func in scrapers:
-#     job, error = func(1, )
-#     results += job
-#     errors += error
-# # Заполняем БД данными полученные со скрайперов
-# for vacancy in results:
-#     """Удаляем city and speciality из скрипта и передаём их в формате инстанс"""
-#     city_txt = vacancy.pop("city")
-#     speciality_txt = vacancy.pop("speciality")
-#     try:
-#         country = City(country_name=city_txt).save()
-#     except Exception:
-#         country = City.objects.filter(country_name=city_txt).first()
-#     try:
-#         prof = Speciality(name_of_specialty=speciality_txt).save()
-#     except Exception:
-#         prof = Speciality.objects.filter(name_of_specialty=speciality_txt).first()
-#     job = Vacancies(**vacancy, city=country, speciality=prof)
-#     try:
-#         job.save()
-#     except Exception as e:
-#         print(e)
-#     if errors:
-#         err = Error(data=errors).save()
-#
-# # # if __name__ == '__main__':
-# # #     print(run())
+from django.test import TestCase
 
+# Create your tests here.
+"""
+скрипт для запуска скриптов вне проекта
+"""
+
+# -*- coding: utf-8 -*-
+import json
+import os
+import sys
+import django
+from django.contrib.auth import get_user_model
+
+#  you have to set the correct path to you settings module
+from django.db import DatabaseError
+from icecream import ic
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service.settings")
+django.setup()
+
+"""модули обязательно должны быть прописаны после 'django.setup()'"""
+from scraping.all_parsers.remote_job import main_scraping_part
+from scraping.all_parsers.hh_ru import get_data
+from scraping.models import Vacancies, City, Speciality, Error
+
+# TODO:написать код, который получает от пользователя city and speciality Done
+# TODO:узнать как получить из query_set нужные данные Done
+# TODO: написать запуск всех скриптов --Done
+# TODO: Понять как заполнить БД полученными данными Done
+# Возвращает пользователя по умолчанию
+User = get_user_model()
+
+"""
+Функция для получения всех городов и специальностей от пользователя
+"""
+
+# def get_info_from_user():
+#     query_set = User.objects.filter(send_email=True)
+#
+#
+# get_info_from_user()
+""" Все скрейперы """
+scrapers = (get_data, main_scraping_part)
+
+
+def get_settings():
+    qs = User.objects.filter(send_email=True).values()
+    settings_lst = set((q['city_id'], q['speciality_id']) for q in qs)
+    return settings_lst
+
+
+print(get_settings())
+
+
+def get_data(_settings):
+    qs = Vacancies.objects.all().values()
+    general_dct = {(q['city_id'], q['speciality_id']) for q in qs}
+    ic(general_dct)
+    speciality_city_list = []
+    for pair in _settings:
+        if pair in general_dct:
+            tmp = {}
+            tmp['city'] = City.objects.filter(id=pair[0]).first().country_name
+            tmp['speciality'] = Speciality.objects.filter(id=pair[1]).first().name_of_specialty
+            speciality_city_list.append(tmp)
+    return speciality_city_list
+
+
+settings = get_settings()
+data_list = get_data(settings, )
+print(data_list.city)
+
+results, errors = [], []
+# Запуск скрейперов
+for func in scrapers:
+    job, error = func(1, )
+    results += job
+    errors += error
+# Заполняем БД данными полученные со скрайперов
+for vacancy in results:
+    """Удаляем city and speciality из скрипта и передаём их в формате инстанс"""
+    city_txt = vacancy.pop("city")
+    speciality_txt = vacancy.pop("speciality")
+    try:
+        country = City(country_name=city_txt).save()
+    except Exception:
+        country = City.objects.filter(country_name=city_txt).first()
+    try:
+        prof = Speciality(name_of_specialty=speciality_txt).save()
+    except Exception:
+        prof = Speciality.objects.filter(name_of_specialty=speciality_txt).first()
+    job = Vacancies(**vacancy, city=country, speciality=prof)
+    try:
+        job.save()
+    except Exception as e:
+        print(e)
+    if errors:
+        err = Error(data=errors).save()
+
+# # if __name__ == '__main__':
+# #     print(run())
--- scraping/views.py
+++ scraping/views.py
@@ -1,11 +1,8 @@
-from django.contrib import messages
 from django.core.paginator import Paginator
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
-from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render
 
-from .forms import Find_job, VForm
+from .forms import Find_job
 from .models import Vacancies, Speciality
 
 
@@ -34,7 +31,7 @@ def list_view(request):
             _filter['speciality__slug'] = speciality
 
         qs = Vacancies.objects.filter(**_filter).select_related('city', 'speciality')
-        paginator = Paginator(qs, 5)  # показывает 5 листов
+        paginator = Paginator(qs, 5)  # показывает 10 листов
 
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
@@ -42,73 +39,66 @@ def list_view(request):
     return render(request, 'scraping/vacant_list.html', context)
 
 
-"""
-Функция для вывода вакансии, которая вас заинтересовала 
-"""
-
-
-# 1-var
-def v_detail(request, pk=None):
-    object_ = get_object_or_404(Vacancies, pk=pk)
-    return render(request, 'scraping/detail.html', {'object': object_})
+#
+# def v_detail(request, pk=None):
+#     # object_ = Vacancy.objects.get(pk=pk)
+#     object_ = get_object_or_404(Vacancy, pk=pk)
+#     return render(request, 'scraping/detail.html', {'object': object_})
 #
 #
-# # 2-var
 # class VDetail(DetailView):
-#     queryset = Vacancies.objects.all()
+#     queryset = Vacancy.objects.all()
 #     template_name = 'scraping/detail.html'
 #     # context_object_name = 'object'
-
-
+#
+#
 # class VList(ListView):
-#     model = Vacancies
-#     template_name = 'scraping/vacant_list.html'
-#     form = Find_job()
+#     model = Vacancy
+#     template_name = 'scraping/list.html'
+#     form = FindForm()
 #     paginate_by = 10
-#     #
+#
 #     def get_context_data(self, **kwargs):
 #         # Call the base implementation first to get a context
 #         context = super().get_context_data(**kwargs)
 #         context['city'] = self.request.GET.get('city')
-#         context['speciality'] = self.request.GET.get('speciality')
+#         context['language'] = self.request.GET.get('language')
 #         context['form'] = self.form
 #
 #         return context
-#
 #     def get_queryset(self):
 #         city = self.request.GET.get('city')
-#         language = self.request.GET.get('speciality')
+#         language = self.request.GET.get('language')
 #         qs = []
 #         if city or language:
 #             _filter = {}
 #             if city:
 #                 _filter['city__slug'] = city
 #             if language:
-#                 _filter['speciality__slug'] = language
-#             qs = Vacancies.objects.filter(**_filter).select_related('city', 'speciality')
+#                 _filter['language__slug'] = language
+#             qs = Vacancy.objects.filter(**_filter).select_related('city', 'language')
 #         return qs
-
-
-class VCreate(CreateView):
-    model = Vacancies
-    # fields = '__all__'
-    form_class = VForm
-    template_name = 'scraping/create.html'
-    success_url = reverse_lazy('home')
-
-
-class VUpdate(UpdateView):
-    model = Vacancies
-    form_class = VForm
-    template_name = 'scraping/create.html'
-    success_url = reverse_lazy('home')
-
-
-class VDelete(DeleteView):
-    model = Vacancies
-    # template_name = 'scraping/delete.html'
-    success_url = reverse_lazy('home')
-
-    def get(self, request, *args, **kwargs):
-        messages.success(request, 'Запись успешно удалена.')
-        return self.post(request, *args, **kwargs)
+#
+# class VCreate(CreateView):
+#     model = Vacancy
+#     # fields = '__all__'
+#     form_class = VForm
+#     template_name = 'scraping/create.html'
+#     success_url = reverse_lazy('home')
+#
+#
+# class VUpdate(UpdateView):
+#     model = Vacancy
+#     form_class = VForm
+#     template_name = 'scraping/create.html'
+#     success_url = reverse_lazy('home')
+#
+#
+# class VDelete(DeleteView):
+#     model = Vacancy
+#     # template_name = 'scraping/delete.html'
+#     success_url = reverse_lazy('home')
+#
+#     def get(self, request, *args, **kwargs):
+#         messages.success(request, 'Запись успешно удалена.')
+#         return self.post(request, *args, **kwargs)
