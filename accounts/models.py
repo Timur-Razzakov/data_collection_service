85fa786aa2a048d11091fe4c90ca30f0db12cc36
Timur-Razzakov <razzakov334@gmail.com>
2023-08-07 13:59:02 +0500

    added serializator and added param 'created_at' for model

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,7 @@ import random
 
 class InviteCode(models.Model):
     code = models.CharField(max_length=6, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.code
@@ -14,6 +15,7 @@ class UserProfile(models.Model):
     phone_number = models.CharField(max_length=15, unique=True)
     invite_code = models.CharField(max_length=6, null=True, blank=True)
     activated_invite_code = models.ForeignKey(InviteCode, on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.phone_number
--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,6 +1,26 @@
-from rest_framework.serializers import *
+from rest_framework import serializers
+
 from models import UserProfile, InviteCode
 
 
-class UserProfileSerializer(Serializer):
-    pass
+class UserProfileSerializer(serializers.ModelSerializer):
+    """для профиля пользователя"""
+
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+
+
+class PhoneNumberSerializer(serializers.Serializer):
+    """для отправки сообщений"""
+    phone_number = serializers.CharField(max_length=15)
+
+
+class VerificationCodeSerializer(serializers.Serializer):
+    """для верификации кода """
+    code = serializers.CharField(max_length=4)
+
+
+class InviteCodeSerializer(serializers.Serializer):
+    """для ввода и активации инвайт-кода """
+    code = serializers.CharField(max_length=6)
